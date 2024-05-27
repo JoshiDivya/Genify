41839ed09d83325a78568b6fd1745920cdf5f27c
Joshi Divya <divyajoshics09@gmail.com>
2024-05-27 10:32:34 +0300

    added signInand signOut on sidebar with links

--- app/(root)/transformations/[id]/add/[type]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddTranformationType = () => {
-  return (
-    <div>AddTranformationType</div>
-  )
-}
-
-export default AddTranformationType
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { usePathname, useRouter } from 'next/navigation'
+import React from 'react'
+
+const TransferTypePage = () => {
+  
+    
+  return (
+    <div>TransferTypePage</div>
+  )
+}
+
+export default TransferTypePage
